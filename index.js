62adef663c771f44ad917c3125f671693f24c17a
Ved Karan Reddy Peesu <vkrpeesu@berkeley.edu>
2025-02-01 20:48:48 -0800

    New button

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// document.getElementById("count").innerText = 5
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+let count = 0
 
-let bonusPoints = 50
-console.log(bonusPoints)
 
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
 
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
