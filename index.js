bdb522c8f025fbbfc546350ea0d3b479764f0b21
Ved Karan Reddy Peesu <vedkrp@gmail.com>
2025-02-01 22:28:14 -0800

    Randomized generation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -10,12 +11,26 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a card?"
@@ -30,7 +45,8 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 7
+    let card = getRandomCard()
     sum += card
+    cards.push(card)
     renderGame()
 }
